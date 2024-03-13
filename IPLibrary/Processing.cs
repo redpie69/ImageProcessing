3e5b221ded3e8812af39c5b2b33125515162afa5
alpay <alpay@DESKTOP-TRFTLKE>
2024-03-13 11:31:13 +0300

    Add project files.

---
+++ IPLibrary/Processing.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace IPLibrary
+{
+    public static partial class IP
+    {
+        public static void FlipHorizontal(Bitmap image)
+        {
+            Size size = image.Size;
+
+            for (int y = 0; y < size.Height; y++)
+            {
+                for (int x = 0; x < size.Width / 2; x++)
+                {
+                    Color temp = image.GetPixel(x, y);
+                    image.SetPixel(x, y, image.GetPixel(size.Width - 1 - x, y));
+                    image.SetPixel(size.Width - 1 - x, y, temp);
+                }
+            }
+        }
+
+        public static void Constrast(Bitmap image, double a)
+        {
+            Size size = image.Size;
+
+            for (int x = 0; x < size.Width; x++)
+            {
+                for (int y = 0; y < size.Height; y++)
+                {
+                    Color color = image.GetPixel(x, y);
+                    double red = color.R * a;
+                    double green = color.G * a;
+                    double blue = color.B * a;
+                    if (red > 255)
+                        red = 255;
+                    else if (red < 0)
+                        red = 0;
+
+                    if (green > 255)
+                        green = 255;
+                    else if (green < 0)
+                        green = 0;
+
+                    if (blue > 255)
+                        blue = 255;
+                    else if (blue < 0)
+                        blue = 0;
+
+                    image.SetPixel(x, y, Color.FromArgb(color.A, (int)red, (int)green, (int)blue));
+                }
+            }
+
+        }
+
+        public static void Brightness(Bitmap image, int a)
+        {
+            Size size = image.Size;
+
+            Color maxColor = MaxColor(image);
+            Color minColor = MinColor(image);
+
+            double maxRed = maxColor.R + a;
+            double maxGreen = maxColor.G + a;
+            double maxBlue = maxColor.B + a;
+
+            double minRed = minColor.R + a;
+            double minGreen = minColor.G + a;
+            double minBlue = minColor.B + a;
+
+
+            for (int x = 0; x < size.Width; x++)
+            {
+                for (int y = 0; y < size.Height; y++)
+                {
+                    Color color = image.GetPixel(x, y);
+                    double red = color.R + a;
+                    double green = color.G + a;
+                    double blue = color.B + a;
+                    if (red > 255)
+                        red = 255;
+                    else if (red < 0)
+                        red = 0;
+
+                    if (green > 255)
+                        green = 255;
+                    else if (green < 0)
+                        green = 0;
+
+                    if (blue > 255)
+                        blue = 255;
+                    else if (blue < 0)
+                        blue = 0;
+
+                    image.SetPixel(x, y, Color.FromArgb(color.A, (int)red, (int)green, (int)blue));
+                }
+            }
+
+        }
+        
+
+        
+    }
+}
---
+++ IPLibrary/Utility.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPLibrary
+{
+    public static partial class IP
+    {
+        private static Color MinColor(Bitmap image)
+        {
+            byte minRed = 255;
+            byte minGreen = 255;
+            byte minBlue = 255;
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color piksel = image.GetPixel(x, y);
+                    if (piksel.R < minRed)
+                        minRed = piksel.R;
+                    if (piksel.G < minGreen)
+                        minGreen = piksel.G;
+                    if (piksel.B < minBlue)
+                        minBlue = piksel.B;
+                }
+
+            }
+            return Color.FromArgb(minRed, minGreen, minBlue);
+        }
+
+        private static Color MaxColor(Bitmap image)
+        {
+            byte maxRed = 255;
+            byte maxGreen = 255;
+            byte maxBlue = 255;
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color piksel = image.GetPixel(x, y);
+                    if (piksel.R > maxRed)
+                        maxRed = piksel.R;
+                    if (piksel.G > maxGreen)
+                        maxGreen = piksel.G;
+                    if (piksel.B > maxBlue)
+                        maxBlue = piksel.B;
+                }
+
+            }
+            return Color.FromArgb(maxRed, maxGreen, maxBlue);
+        }
+
+        private static void Complement(Bitmap image)
+        {
+            Color oldColor;
+            Color newColor;
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    oldColor = image.GetPixel(x, y);
+                    newColor = Color.FromArgb(255 - oldColor.R, 255 - oldColor.G, 255 - oldColor.B);
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+        }
+    }
+}
