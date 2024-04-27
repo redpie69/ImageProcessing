4c133b22cab5426eef7f35ef519b888627e2ab12
alpay aldığ <alpayaldig@outlook.com>
2024-04-27 23:13:36 +0300

    histogram germe genisletme

--- IPLibrary/Processing.cs
+++ IPLibrary/Processing.cs
@@ -404,5 +404,64 @@ namespace IPLibrary
             
         }
 
+        public static void HistogramGerme(Bitmap image, int max,int min)
+        {
+            RGB2GrayScale(image);
+
+            int imgMin = 255;
+            int imgMax = 0;
+
+            for(int y=0;y<image.Height;y++) 
+            {
+                for(int x=0;x< image.Width;x++)
+                {
+                    if(image.GetPixel(x, y).R < imgMin)
+                        imgMin = image.GetPixel(x,y).R;
+                    if(image.GetPixel(x,y).R > imgMax)
+                        imgMax = image.GetPixel(x,y).R;
+                }
+            }
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    double oldPixel = image.GetPixel(x, y).R;
+                    double newPixel = (oldPixel - imgMin) * (max - min) / (imgMax - imgMin) + min;
+                    Color newColor = Color.FromArgb((int)newPixel,(int)newPixel,(int)newPixel);
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+        public static void HistogramGenisletme(Bitmap image)
+        {
+            RGB2GrayScale(image);
+
+            double imgMin = 255;
+            double imgMax = 0;
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    if (image.GetPixel(x, y).R < imgMin)
+                        imgMin = image.GetPixel(x, y).R;
+                    if (image.GetPixel(x, y).R > imgMax)
+                        imgMax = image.GetPixel(x, y).R;
+                }
+            }
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    double oldPixel = image.GetPixel(x, y).R;
+                    double newPixel = (oldPixel - imgMin) / (imgMax - imgMin) * 255;
+                    Color newColor = Color.FromArgb((int)newPixel, (int)newPixel, (int)newPixel);
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+        }
     }
 }
